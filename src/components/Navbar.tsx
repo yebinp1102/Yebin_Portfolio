7cc0e0b0439175b7b9846a9d4f85b70e22daeda8
yebin <yebinp1102@naver.com>
2024-03-30 20:30:46 +0900

    Feat: Navigation toggle update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from "../assets/images/home.svg";
 import User from "../assets/images/user.png";
-import Pen from '../assets/images/pen.png';
+import Pen from "../assets/images/pen.png";
 import Mail from "../assets/images/mail.png";
 import Docs from "../assets/images/document.png";
 
@@ -17,49 +17,122 @@ type Props = {
   contactRef: React.RefObject<HTMLDivElement>;
 };
 
-const Navbar = ({ scrollToSection, bannerRef, aboutRef, skillsRef, projectsRef, contactRef}: Props) => {
+const Navbar = ({
+  scrollToSection,
+  bannerRef,
+  aboutRef,
+  skillsRef,
+  projectsRef,
+  contactRef,
+}: Props) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [isHomeHovered, setIsHomeHovered] = useState<boolean>(false);
+  const [isAboutHovered, setIsAboutHovered] = useState<boolean>(false);
+  const [isSkillHovered, setIsSkillHovered] = useState<boolean>(false);
+  const [isProjectHovered, setIsProjectHovered] = useState<boolean>(false);
+  const [isContactHovered, setIsContactHovered] = useState<boolean>(false);
+
   const buttons = [
     {
       icon: Home,
       ref: bannerRef,
+      text: "Home",
+      state: isHomeHovered,
+      stateFnc: setIsHomeHovered,
     },
     {
       icon: User,
       ref: aboutRef,
+      text: "About",
+      state: isAboutHovered,
+      stateFnc: setIsAboutHovered,
     },
     {
       icon: Pen,
-      ref: skillsRef
+      ref: skillsRef,
+      text: "Skills",
+      state: isSkillHovered,
+      stateFnc: setIsSkillHovered,
     },
     {
       icon: Docs,
-      ref: projectsRef
+      ref: projectsRef,
+      text: "Project",
+      state: isProjectHovered,
+      stateFnc: setIsProjectHovered,
     },
     {
       icon: Mail,
-      ref: contactRef
-    }
+      ref: contactRef,
+      text: "Contact",
+      state: isContactHovered,
+      stateFnc: setIsContactHovered,
+    },
   ];
 
-  return (
-    <motion.header
-      className="bottom-10 right-10 fixed z-[100]"
-      initial={{ opacity: 0, y: 100 }}
-      whileInView={{ opacity: 1, y: "0" }}
-      transition={{ ease: "backIn", duration: 0.5, delay: 3.5 }}
-    >
-      <menu className="flex flex-col items-center gap-5">
+  const Menu = () => {
+    return (
+      <>
         {buttons.map((button) => (
-          <div className="bg-point cursor-pointer rounded-full border border-white shadow-lg w-[50px] h-[50px] p-2 hover:scale-110">
-            <img
-              src={button.icon}
-              onClick={() => scrollToSection(button.ref)}
-              className="w-full object-cover"
-            />
+          <div
+            onMouseEnter={() => button.stateFnc(true)}
+            onMouseLeave={() => button.stateFnc(false)}
+            onClick={() => scrollToSection(button.ref)}
+            className="bg-point hover:bg-pink-500 cursor-pointer rounded-full border border-white shadow-lg w-[50px] h-[50px] p-2 hover:scale-110 transition flex items-center justify-center"
+          >
+            {button.state ? (
+              <p className="text-white text-xs font-semibold">{button.text}</p>
+            ) : (
+              <img src={button.icon} className="w-[90%] object-cover" />
+            )}
           </div>
         ))}
-      </menu>
-    </motion.header>
+      </>
+    );
+  };
+
+  useEffect(() => {
+    setTimeout(() => setShowMenu(true), 5000);
+  }, []);
+
+  return (
+    <header className="bottom-10 right-10 fixed z-[100]">
+      <motion.menu
+        className="flex flex-col items-center gap-4 transition"
+        initial={{ opacity: 0, y: 10 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "backIn", duration: 0.5, delay: 3.5 }}
+      >
+        <div className="relative w-[55px] h-[335px] py-[2.5px] overflow-hidden">
+            <div className={`flex flex-col items-center gap-5 transition transform ${showMenu ? 'translate-y-0' : 'translate-y-[333px]'}`}>
+              {buttons.map((button) => (
+                <div
+                  onMouseEnter={() => button.stateFnc(true)}
+                  onMouseLeave={() => button.stateFnc(false)}
+                  onClick={() => scrollToSection(button.ref)}
+                  className="bg-point hover:bg-pink-500 cursor-pointer rounded-full border border-white shadow-lg w-[50px] h-[50px] p-2 hover:scale-110 transition flex items-center justify-center"
+                >
+                  {button.state ? (
+                    <p className="text-white text-xs font-semibold">
+                      {button.text}
+                    </p>
+                  ) : (
+                    <img src={button.icon} className="w-[90%] object-cover" />
+                  )}
+                </div>
+              ))}
+            </div>
+          
+        </div>
+
+        <div
+          className="bg-point text-white text-3xl hover:bg-pink-500 cursor-pointer rounded-full border border-white shadow-lg w-[51px] h-[51px] p-2 hover:scale-110 transition flex items-center justify-center"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          {showMenu ? <p>&#8681;</p> : <p className=" rotate-180">&#8681;</p>}
+        </div>
+      </motion.menu>
+    </header>
   );
 };
 
